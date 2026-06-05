138afa3d8ce1cab5a34da1c40d2f87a9c6e47ed1
BerCorradi <bernardo.c.oliveira26@gmail.com>
2026-06-05 18:20:13 -0300

    implementacao subclasses Cliente e Lojista com construtores e super

--- src/com/ufla/usuarios/Cliente.java
+++ src/com/ufla/usuarios/Cliente.java
@@ -5,6 +5,34 @@ public class Cliente extends Usuario{
 	private String telefone;
 	private String endereco;
 	
+	public Cliente(int id, String nomeUsuario, String email, String senha, String cpf, String telefone, String endereco) {
+		super(id, nomeUsuario, email, senha);
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.endereco = endereco;
+	}
+
+	//CPF
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	//Telefone
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	//Endereco
+	public String getEndereco() {
+		return endereco;
+	}
+	
 	public void cadastrar() {
 		
 	}
--- src/com/ufla/usuarios/Lojista.java
+++ src/com/ufla/usuarios/Lojista.java
@@ -3,7 +3,20 @@ package com.ufla.usuarios;
 public class Lojista extends Usuario {
 	private String cnpj;
 	
+	public Lojista(int id, String nomeUsuario, String email, String senha, String cnpj) {
+		super(id, nomeUsuario, email, senha);
+		this.cnpj = cnpj;
+	}
 	
+	//CNPJ
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
 	public void gerenciarProdutos() {
 		
 	}
