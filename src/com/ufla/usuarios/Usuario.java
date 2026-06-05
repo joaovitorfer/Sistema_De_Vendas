d39e6ddf3fac205d1add03e2a096fa592d451eed
BerCorradi <bernardo.c.oliveira26@gmail.com>
2026-06-05 18:05:38 -0300

    classe Usuario com abstracao e encapsulamento

--- src/com/ufla/usuarios/Usuario.java
+++ src/com/ufla/usuarios/Usuario.java
@@ -1,17 +1,63 @@
 package com.ufla.usuarios;
 
-public class Usuario {
+public abstract class Usuario {
 	private int id;
 	private String nomeUsuario;
 	private String email;
 	private String senha;
 	
+	public Usuario(int id, String nomeUsuario, String email, String senha){
+			this.id = id;
+			this.nomeUsuario = nomeUsuario;
+			setEmail(email);
+			this.senha = senha;
+	}
+
+	//ID'
+	public int getId(){
+		return id;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	//Nome de usuario
+		public String getNomeUsuario(){
+		return nomeUsuario;
+	}
 	
+	public void setNomeUsuario(String nomeUsuario){
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	//Email
+		public String getEmail(){
+		return email;
+	}
+	
+		public void setEmail(String email){
+			if (email != null && email.contains("@")){
+				this.email = email;
+		} else {
+			System.out.println("Formato de email invalido");
+		}
+	}
+	
+	//Senha
+		public String getSenha(){
+		return senha;
+	}
+
+	public void setSenha(String senha){
+		this.senha = senha;
+	}
+
 	public void login() {
 		
 	}
 	
 	public void logout() {
-		
+		System.out.println(nomeUsuario + " encerrou a sessão.");
 	}
 }
