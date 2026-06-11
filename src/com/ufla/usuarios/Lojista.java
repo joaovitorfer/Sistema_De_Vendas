6534570aa92ddb4310b6ec7e3cc29eac0b0e1d22
João Pedro Goulart de Faria <jpgfaria.cruzeiro@gmail.com>
2026-06-11 13:55:52 -0300

    Update Lojista.java

--- src/com/ufla/usuarios/Lojista.java
+++ src/com/ufla/usuarios/Lojista.java
@@ -1,26 +1,64 @@
 package com.ufla.usuarios;
 
+/**
+ * Representa um lojista do sistema.
+ *
+ * <p>A classe Lojista herda os atributos e comportamentos da classe
+ * {@code Usuario} e adiciona informações específicas relacionadas ao
+ * vendedor, como o CNPJ. Além disso, disponibiliza funcionalidades para
+ * cadastro de produtos e atualização de estoque.</p>
+ *
+ * @author SeuNome
+ * @version 1.0
+ */
 public class Lojista extends Usuario {
-	private String cnpj;
-	
-	public Lojista(int id, String nomeUsuario, String email, String senha, String cnpj) {
-		super(id, nomeUsuario, email, senha);
-		this.cnpj = cnpj;
-	}
-	
-	//CNPJ
-	public String getCnpj() {
-		return cnpj;
-	}
 
-	public void setCnpj(String cnpj) {
-		this.cnpj = cnpj;
-	}
-	public void cadastrarProduto() {
-		System.out.println("Produto cadastrado.");
-	}
+    /** CNPJ do lojista. */
+    private String cnpj;
 
-	public void atualizarEstoque() {
-		System.out.println("Estoque atualizado.");
-	}
+    /**
+     * Constrói um novo lojista.
+     *
+     * @param id Identificador único do usuário.
+     * @param nomeUsuario Nome de usuário do lojista.
+     * @param email E-mail do lojista.
+     * @param senha Senha de acesso ao sistema.
+     * @param cnpj CNPJ da empresa ou vendedor.
+     */
+    public Lojista(int id, String nomeUsuario, String email, String senha, String cnpj) {
+        super(id, nomeUsuario, email, senha);
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * Retorna o CNPJ do lojista.
+     *
+     * @return CNPJ do lojista.
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * Define o CNPJ do lojista.
+     *
+     * @param cnpj Novo CNPJ do lojista.
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * Realiza o cadastro de um novo produto no sistema.
+     */
+    public void cadastrarProduto() {
+        System.out.println("Produto cadastrado.");
+    }
+
+    /**
+     * Atualiza a quantidade de produtos disponíveis em estoque.
+     */
+    public void atualizarEstoque() {
+        System.out.println("Estoque atualizado.");
+    }
 }
